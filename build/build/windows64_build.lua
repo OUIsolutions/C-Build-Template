001cc9992dd3a51f967e7165e8fff2e613b3f0a6
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-02-15 22:17:22 -0300

    added windows 64

---
+++ build/build/windows64_build.lua
@@ -0,0 +1,20 @@
+local windows_build_done = false
+function windowsi64_build()
+    if windows_build_done then
+        return
+    end
+    windows_build_done = false
+    os.execute("mkdir -p release")
+
+    local image = darwin.ship.create_machine("debian:latest")
+    image.add_comptime_command("apt-get update")
+    image.add_comptime_command("apt-get -y install mingw-w64")
+
+    image.start({
+        volumes = {
+            { "./release", "/release" },
+            { "./src",     "/src" }
+        },
+        command = "x86_64-w64-mingw32-gcc --static /src/main.c -o /release/windows64.exe"
+    })
+end
--- build/main.lua
+++ build/main.lua
@@ -5,7 +5,9 @@ local build_funcs = {
     amalgamation_build = amalgamation_build,
     alpine_static_build = alpine_static_build,
     rpm_static_build = rpm_static_build,
-    windowsi32_build = windowsi32_build
+    windowsi32_build = windowsi32_build,
+    windowsi64_build = windowsi64_build
+
 }
 
 function main()
