9e5909a3a0e119622448320bdff9ae4990b875be
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-02-17 04:05:26 -0300

    att

--- testage/fedora.lua
+++
@@ -1,9 +0,0 @@
-
-local image = darwin.ship.create_machine("fedora:latest")
-image.provider = "docker"
-image.start({
-    flags = {"-it"},
-    volumes = {
-        { "./release", "/release" },
-    },
-})
